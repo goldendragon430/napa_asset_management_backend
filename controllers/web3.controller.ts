6519b42b63f6451488c9b7236ac9021da2373193
VivekJd-18 <vivekdhage.vhits@gmail.com>
2023-04-14 19:18:52 +0530

    added 1. transaction history, 2. wallet balance - both (1. custom and 2. native), 3. create Wallet

--- controllers/web3.controller.ts
+++ controllers/web3.controller.ts
@@ -1,17 +1,173 @@
 // web3 functions will come in this file
 
 import ApiResponse from "../utils/api-response";
+import Moralis from "moralis"
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+import { ethers } from "ethers"
+import commonTokenAbi from "../web3Utils/abis/tokenAbi.json"
 
-const walletBalance = async (req, res) => {
+// 1. transaction history - DONE
+// 2. wallet balance - both (1. custom and 2. native) DONE
+// 3. create Wallet - Done
+// 4. send & Receive Tokens
+// 5. switching chain.
+// 6.import tokens
+// 7. import wallet.
+// 8. Import NFTs
+
+
+
+const transactionHistory = async (req, res) => {
   try {
+    await Moralis.start({
+      apiKey: "D8Kfm2KtjFHVEpqvPmTVgaNLvY8TFEhrIBi8h71wjcTfFIdlmSKFlYJcEGATK8dr"
+    });
+
+    const response = await Moralis.EvmApi.transaction.getWalletTransactions({
+      "chain": (req.params.chainId).toString(),
+      "address": (req.params.walletAddress).toString()
+    });
+
+    console.log(response, `All Transactions of ${(req.params.walletAddress).toString()}.`);
+
     ApiResponse.successResponseWithData(
       res,
-      "Wallet Balance Get Successfully",
-      { address: req.params.walletId }
+      "Transactions fetched successfully",
+      { transactions: response }
     );
   } catch (error) {
+    console.log(error, "Error while Fetching transactions");
     res.status(503).send();
   }
 };
 
-module.exports = { walletBalance };
+
+const nativeTokenWalletBalance = async (req, res) => {
+  try {
+    await Moralis.start({
+      apiKey: "D8Kfm2KtjFHVEpqvPmTVgaNLvY8TFEhrIBi8h71wjcTfFIdlmSKFlYJcEGATK8dr"
+    });
+
+    const address = req.params.walletAddress;
+
+    let chain;
+    if (req.params.chainId == 0) {
+      chain = EvmChain.ETHEREUM;
+    } else if (req.params.chainId == 1) {
+      chain = EvmChain.GOERLI;
+    } else if (req.params.chainId == 2) {
+      chain = EvmChain.SEPOLIA;
+    }
+    const response = await Moralis.EvmApi.balance.getNativeBalance({
+      address,
+      chain
+    });
+
+    console.log(response, `Balance of ${(req.params.walletAddress).toString()}. ${response}`);
+
+    ApiResponse.successResponseWithData(
+      res,
+      "Balance fetched successfully",
+      { transactions: response }
+    );
+  } catch (error) {
+    console.log(error, "Error while Fetching balance");
+    res.status(503).send();
+  }
+};
+
+const customTokenWalletBalance = async (req, res) => {
+  try {
+    await Moralis.start({
+      apiKey: "D8Kfm2KtjFHVEpqvPmTVgaNLvY8TFEhrIBi8h71wjcTfFIdlmSKFlYJcEGATK8dr"
+    });
+
+    let chain;
+    if (req.params.chainId == 0) {
+      chain = EvmChain.ETHEREUM;
+    } else if (req.params.chainId == 1) {
+      chain = EvmChain.GOERLI;
+    } else if (req.params.chainId == 2) {
+      chain = EvmChain.SEPOLIA;
+    }
+
+    const response = await Moralis.EvmApi.token.getWalletTokenBalances({
+      "chain": chain,
+      "tokenAddresses": [(req.params.tknAddress).toString()],
+      "address": req.params.walletAddress
+    });
+
+    console.log(response, `Balance of ${(req.params.walletAddress).toString()}. ${response}`);
+
+    ApiResponse.successResponseWithData(
+      res,
+      "Balance fetched for Custom Token successfully",
+      { transactions: response }
+    );
+  } catch (error) {
+    console.log(error, "Error while Fetching Custom Token balance");
+    res.status(503).send();
+  }
+};
+
+
+const createWallet = async (req, res) => {
+  try {
+
+    const response = ethers.Wallet.createRandom();
+
+    console.log(response, "Wallet creation response.");
+
+    ApiResponse.successResponseWithData(
+      res,
+      "Wallet created successfully",
+      { wallet: response }
+    );
+  } catch (error) {
+    console.log(error, "Error while Fetching creating a Wallet");
+    res.status(503).send();
+  }
+};
+
+// const sendNativeToken = async (req, res) => {
+//   try {
+//     // /sendNativeToken/:contract_address/:send_token_amount/:to_address/:send_account/:private_key
+//     window.ethersProvider.getGasPrice().then(async (currentGasPrice: any) => {
+//       let gas_price = ethers.hexlify((currentGasPrice))
+//       let gas_limit: any = 100000
+//       const tx = {
+//         from: req.params.send_account,
+//         to: req.params.to_address,
+//         value: ethers.parseEther(req.params.send_token_amount),
+//         nonce: window.ethersProvider.getTransactionCount(
+//           req.params.send_account,
+//           "latest"
+//         ),
+
+//         gasLimit: ethers.hexlify(gas_limit), // 100000
+//         gasPrice: gas_price,
+//       }
+
+//       try {
+//         let wallet = new ethers.Wallet(req.params.private_key)
+//         let walletSigner = wallet.connect(window.ethersProvider)
+//         const response: any = await walletSigner.sendTransaction(tx)
+//         alert("Send finished!")
+//         ApiResponse.successResponseWithData(
+//           res,
+//           "Tokens sent successfully.",
+//           { wallet: response }
+//         );
+//       } catch (error) {
+//         alert("failed to send!!")
+//       }
+
+//     }).catch(() => {
+//     })
+//   } catch (error) {
+//     console.log(error, "Error while Sending Tokens");
+//     res.status(503).send();
+//   }
+// };
+
+module.exports = { transactionHistory, nativeTokenWalletBalance, customTokenWalletBalance, createWallet };
--- routes/web3.routes.ts
+++ routes/web3.routes.ts
@@ -3,6 +3,10 @@ import express from "express";
 const Web3Controller = require("../controllers/web3.controller");
 const router = express.Router();
 
-router.get("/walletBalance/:walletId", Web3Controller.walletBalance);
+router.get("/Transactions/:chainId/:walletAddress", Web3Controller.transactionHistory);
+router.get("/nativeBalance/:chainId/:walletAddress", Web3Controller.nativeTokenWalletBalance);
+router.get("/customBalance/:chainId/:walletAddress/:tknAddress", Web3Controller.customTokenWalletBalance);
+router.get("/createWallet", Web3Controller.createWallet);
+router.get("/sendNativeToken/:contract_address/:send_token_amount/:to_address/:send_account/:private_key", Web3Controller.createWallet);
 
 module.exports = router;
