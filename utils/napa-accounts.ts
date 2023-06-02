65b4ba49d8330c3ca3da010858d4f9da429e0013
hammadyounas <m.hammadyounas@outlook.com>
2023-06-02 12:55:37 +0500

    get phrase and private key by profileId functions updated

--- utils/napa-accounts.ts
+++ utils/napa-accounts.ts
@@ -1,15 +1,21 @@
-import { napaDB } from "../index";
+import axios from "axios";
 import { decryptString } from "./encryption";
 
 export const getPhraseByProfileId = async (profileId: string) => {
-  const sql = `SELECT napaWalletAccountPhrase FROM napa_accounts WHERE profileId = "${profileId}"`;
-  const [napaAccount] = await napaDB.execute(sql);
-  return decryptString(napaAccount[0]?.napaWalletAccountPhrase);
+  const options = {
+    method: "GET",
+    url: `https://napa-backend-staging.napasociety.io/getPhraseByProfileId?profileId=${profileId}`,
+  };
+  const response = await axios(options);
+  return decryptString(response?.data?.data);
 };
 
 export const getPrivateKeyByProfileId = async (profileId: string) => {
-  const sql = `SELECT * FROM napa_accounts WHERE profileId = "${profileId}"`;
-  const [napaAccount] = await napaDB.execute(sql);
-  const activeWalletAC = napaAccount[0]?.activeWalletAC
-  return decryptString(napaAccount[0][`NWA_${activeWalletAC}_PK`]);
+  const options = {
+    method: "GET",
+    url: `https://napa-backend-staging.napasociety.io/getPrivateKeyByProfileId?profileId=${profileId}`,
+  };
+  const response = await axios(options);
+  const activeWalletAC = response?.data?.data?.activeWalletAC
+  return decryptString(response?.data?.data[`NWA_${activeWalletAC}_PK`]);
 };
