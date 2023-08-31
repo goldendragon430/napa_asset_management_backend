207b9e2082ceeec49570c477e981631c7b18e683
hammadyounas <m.hammadyounas@outlook.com>
2023-08-31 16:41:00 +0500

    added consoles for testing

--- controllers/webhook.controller.ts
+++ controllers/webhook.controller.ts
@@ -7,15 +7,21 @@ const webhook = async (req, res) => {
   try {
     console.log(body);
     if (body?.erc20Transfers[0]) {
+      console.log("body?.erc20Transfers[0]=====>>>>>", body?.erc20Transfers[0]);
+      
       const getSenderProfileId = `SELECT profileId FROM napa_accounts WHERE NWA_1_AC = "${body?.erc20Transfers[0]?.from}" OR NWA_2_AC = "${body?.erc20Transfers[0]?.from}" OR NWA_3_AC = "${body?.erc20Transfers[0]?.from}" OR NWA_4_AC = "${body?.erc20Transfers[0]?.from}" OR NWA_5_AC = "${body?.erc20Transfers[0]?.from}"`;
       const [senderProfileId] = await napaDB.query(getSenderProfileId)
       const getSenderDeviceToken = `SELECT deviceToken from users WHERE profileId = "${senderProfileId[0]?.profileId}"`
-      const [senderDeviceToken] = await napaDB.query(getSenderDeviceToken)
+      const [senderDeviceToken] = await napaDB.query(getSenderDeviceToken)      
 
       const getReceiverProfileId = `SELECT profileId FROM napa_accounts WHERE NWA_1_AC = "${body?.erc20Transfers[0]?.to}" OR NWA_2_AC = "${body?.erc20Transfers[0]?.to}" OR NWA_3_AC = "${body?.erc20Transfers[0]?.to}" OR NWA_4_AC = "${body?.erc20Transfers[0]?.to}" OR NWA_5_AC = "${body?.erc20Transfers[0]?.to}"`;
       const [receiverProfileId] = await napaDB.query(getReceiverProfileId)
       const getReceiverDeviceToken = `SELECT deviceToken from users WHERE profileId = "${receiverProfileId[0]?.profileId}"`
       const [receiverDeviceToken] = await napaDB.query(getReceiverDeviceToken)
+
+      console.log("senderInfo====>>>", senderProfileId[0]?.profileId, senderDeviceToken[0]?.deviceToken);
+      console.log("receiverInfo====>>>", receiverProfileId[0]?.profileId, receiverDeviceToken[0]?.deviceToken);
+      
 
       sendNotification(senderDeviceToken[0]?.deviceToken,`${body?.erc20Transfers[0]?.tokenSymbol} sent`,`Send Transaction ${body?.erc20Transfers[0]?.value / 10 ** 18} ${body?.erc20Transfers[0]?.tokenSymbol} is Complete`)
 
